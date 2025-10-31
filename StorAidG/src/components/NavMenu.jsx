f623730d5fa72b81af1f8a381ddc34935bc460a6
Richard Karth <richard.k.triumph@gmail.com>
2025-10-31 20:35:51 +0100

    Finished with the basic navmenu CSS

--- StorAidG/src/components/NavMenu.jsx
+++ StorAidG/src/components/NavMenu.jsx
@@ -2,7 +2,22 @@ import React from 'react'
 
 const NavMenu = () => {
   return (
-    <div>NavMenu</div>
+    <div className='nav-menu'>
+      <div>
+        <img src="../src/assets/Logo-icon.svg" alt="" />
+      </div>
+
+      <div className='nav-menu-options'>
+          <p>HOME</p>
+          <p>ABOUT US</p>
+          <p>SERVICES</p>
+          <p>CONTACT US</p>
+      </div>
+
+      <div>
+        <button className='nav-menu-button'>BOOK NOW</button>
+      </div>
+    </div>
   )
 }
 
