5c596e93334b8b7975a6b7f41d908aaec5b7b0c9
Richard Karth <richard.k.triumph@gmail.com>
2025-10-31 15:42:22 +0100

    structured and added the components in app.jsx

--- StorAidG/src/App.jsx
+++ StorAidG/src/App.jsx
@@ -2,13 +2,81 @@ import { useState } from 'react'
 import reactLogo from './assets/react.svg'
 import viteLogo from '/vite.svg'
 import './App.css'
+import TopBar from './components/TopBar'
+import AboutUs from './components/AboutUs'
+import AllRightsReserved from './components/AllRightsReserved'
+import BookNow from './components/BookNow'
+import LatestBlogs from './components/LatestBlogs'
+import Logos from './components/Logos'
+import NavMenu from './components/NavMenu'
+import NewsLetter from './components/NewsLetter'
+import OurServices from './components/OurServices'
+import PricingPlan from './components/PricingPlan'
+import Testimonials from './components/Testimonials'
+import TopBar from './components/TopBar'
+import WelcomePage from './components/WelcomePage'
+import WhyChooseUs from './components/WhyChooseUs'
+
 
 function App() {
   
 
   return (
-    <>
-    </>
+    <div className='container'>
+
+      <header>
+        <TopBar />
+        <NavMenu />
+      </header>
+      
+      <main>
+        <section className='hero'>
+          <WelcomePage></WelcomePage>
+        </section>
+
+        <section className='about-us'>
+        <AboutUs />
+        </section>
+
+        <section className='logos'>
+        <Logos />
+        </section>
+
+        <section className='our-services'>
+        <OurServices />
+        </section>
+        
+        <section className='testimonials'>
+        <Testimonials />
+        </section>
+
+        <section className='why-choose-us'>
+        <WhyChooseUs />
+        </section>
+
+        <section className='pricing-plan'>
+        <PricingPlan />
+        </section>
+
+        <section className='book-now'>
+        <BookNow />
+        </section>
+
+        <section className='latest-blogs'>
+        <LatestBlogs />
+        </section>
+
+        <section className='newsletter'>
+        <NewsLetter />
+        </section>
+
+      </main>
+
+      <footer className='rights-reserved'>
+        <AllRightsReserved />
+      </footer>
+      
+    </div>
   )
 }
 
---
+++ StorAidG/src/components/AllRightsReserved.jsx
@@ -0,0 +1,9 @@
+import React from 'react'
+
+const AllRightsReserved = () => {
+  return (
+    <div>AllRightsReserved</div>
+  )
+}
+
+export default AllRightsReserved
