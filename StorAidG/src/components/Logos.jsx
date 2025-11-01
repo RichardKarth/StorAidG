217e71058ab28e12d3cf6e005eb467cef7e86c9b
Richard Karth <richard.k.triumph@gmail.com>
2025-11-01 18:20:04 +0100

    Finished the css for logos section

--- StorAidG/src/components/Logos.jsx
+++ StorAidG/src/components/Logos.jsx
@@ -2,7 +2,16 @@ import React from 'react'
 
 const Logos = () => {
   return (
-    <div>Logos</div>
+    <div className='logos-wrapper'>
+      <div className='logos-items'>
+        <img src="../src/assets/Logoipsum 1.svg" alt="" />
+        <img src="../src/assets/Logoipsum 2.svg" alt="" />
+        <img src="../src/assets/Logoipsum 3.svg" alt="" />
+        <img src="../src/assets/Logoipsum 4.svg" alt="" />
+        <img src="../src/assets/Logoipsum 5.svg" alt="" />
+      </div>
+
+    </div>
   )
 }
 
