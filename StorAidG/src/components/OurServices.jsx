4b40d38ca6a07464aee1d5e52138369f0b0d7d27
Richard Karth <richard.k.triumph@gmail.com>
2025-11-02 09:33:16 +0100

    Finished the css for the our-services section

--- StorAidG/src/components/OurServices.jsx
+++ StorAidG/src/components/OurServices.jsx
@@ -2,7 +2,50 @@ import React from 'react'
 
 const OurServices = () => {
   return (
-    <div>OurServices</div>
+    
+      <div className='margin-wrapper'>
+        <div className='our-services'>
+          <div className='our-services-upper-box'>
+            <div className='our-services-upper-left-box'>
+              <h4>Our Services</h4>
+              <h3>Specialized Storage for Every Special Item</h3>
+            </div>
+            <div className='our-services-upper-right-box'>
+              <p>Lorem ipsum dolor sit amet, consectetur adipiscing elit. Quisque molestie nisl sed dui lacinia gravida. Nulla quis nulla leo. Mauris ac blandit nisi non sodales augue. Phasellus eget elit gravida.</p>
+            </div>
+          </div>
+
+          <div className='our-services-lower-box'>
+
+            <div className='our-services-lower-box-1'>
+              <div className='diverse-unit-size-box'>
+                <h4>Diverse Unit Sizes</h4>
+                <p>Lorem ipsum dolor sit amet, consectetur adipiscing elit. Ut elit tellus, luctus nec ullamcorper mattis, pulvinar dapibus leo.</p>
+              </div>
+              <div className='moving-assistance-box'>
+                <h4>Moving Assistance</h4>
+                <p>Lorem ipsum dolor sit amet, consectetur adipiscing elit. Ut elit tellus, luctus nec ullamcorper mattis, pulvinar dapibus leo.</p>
+              </div>
+            </div>
+
+            <div className='our-services-lower-box-2'>
+              <div className='our-services-square'></div>
+            </div>
+
+            <div className='our-services-lower-box-3'>
+              <div className='vehicle-storage-box'>
+                <h4>Vehicle Storage</h4>
+                <p>Lorem ipsum dolor sit amet, consectetur adipiscing elit. Ut elit tellus, luctus nec ullamcorper mattis, pulvinar dapibus leo.</p>
+              </div>
+              <div className='top-notch-security-box'>
+                <h4>Top-Notch Security</h4>
+                <p>Lorem ipsum dolor sit amet, consectetur adipiscing elit. Ut elit tellus, luctus nec ullamcorper mattis, pulvinar dapibus leo.</p>
+              </div>
+            </div>
+          </div>
+        </div>
+
+      </div>
   )
 }
 
