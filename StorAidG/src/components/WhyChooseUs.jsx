535c22163eede30a244dee0bba5458ea7dfa200e
Richard Karth <richard.k.triumph@gmail.com>
2025-11-05 09:07:59 +0100

    Finished styling the css section for WhyChooseUs

--- StorAidG/src/components/WhyChooseUs.jsx
+++ StorAidG/src/components/WhyChooseUs.jsx
@@ -2,7 +2,67 @@ import React from 'react'
 
 const WhyChooseUs = () => {
   return (
-    <div>WhyChooseUs</div>
+    <div className='why-us-wrapper'>
+      <div className='margin-wrapper-bg'>
+        <div className='why-us-container'>
+
+          <div className='why-us-left-side'>
+            <div>
+
+              <div className='why-us'>
+                <h4>Why Choose Us</h4>
+                <h3>Choose Us for Exceptional Storage Solutions</h3>
+              </div>
+
+              <div className='why-choose-us-information'>
+                <img src="../src/assets/Lock-icon.svg" alt="" />
+                <div>
+                  <h5>Security and Safety</h5>
+                  <p>Lorem ipsum dolor sit amet, consectetur adipiscing elit. Ut elit tellus, luctus nec ullamcorper mattis, pulvinar dapibus leo.</p>
+                </div>
+              </div>
+
+              <div className='why-choose-us-information'>
+                <img src="../src/assets/Money-bag-icon.svg" alt="" />
+
+                <div>
+                  <h5>Flexible and Affordable</h5>
+                  <p>Lorem ipsum dolor sit amet, consectetur adipiscing elit. Ut elit tellus, luctus nec ullamcorper mattis, pulvinar dapibus leo.</p>
+                </div>
+
+              </div>
+
+              <div className='why-choose-us-information'>
+                <img src="../src/assets/Broom-icon.svg" alt="" />
+
+                <div>
+                  <h5>Clean and Well-Maintained Facilities</h5>
+                  <p>Lorem ipsum dolor sit amet, consectetur adipiscing elit. Ut elit tellus, luctus nec ullamcorper mattis, pulvinar dapibus leo.</p>
+                </div>
+              </div>
+
+              <div className='why-choose-us-information'>
+                <img src="../src/assets/Clock-icon.svg" alt="" />
+
+                <div>
+                  <h5>24/7 and Convenient Access</h5>
+                  <p>Lorem ipsum dolor sit amet, consectetur adipiscing elit. Ut elit tellus, luctus nec ullamcorper mattis, pulvinar dapibus leo.</p>
+                </div>
+              </div>
+
+            </div>
+          </div>
+          <div className='why-us-right-side'>
+
+            <div>
+              <p>Lorem ipsum dolor sit amet, consectetur adipiscing elit. Quisque molestie nisl sed dui lacinia gravida. Nulla quis nulla leo. Mauris ac blandit nisi non sodales augue. Phasellus eget elit gravida.</p>
+            </div>
+            <div className='why-us-square'></div>
+
+          </div>
+        </div>
+      </div>
+    </div>
   )
 }
 
