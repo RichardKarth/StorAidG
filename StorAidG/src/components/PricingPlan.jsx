72ef26d69a61ff77b5dfeb39452e017ef174bc51
Richard Karth <richard.k.triumph@gmail.com>
2025-11-05 18:11:37 +0100

    Finished the basic styling for the pricing plan section

--- StorAidG/src/components/PricingPlan.jsx
+++ StorAidG/src/components/PricingPlan.jsx
@@ -2,7 +2,150 @@ import React from 'react'
 
 const PricingPlan = () => {
   return (
-    <div>PricingPlan</div>
+    <div className='margin-wrapper'>
+      <div className='price-plan-top-div'>
+        <h4>Pricing Plan</h4>
+        <h3>Find the Perfect Plan for Your Storage Needs</h3>
+      </div>
+
+      <div className='price-plan-bottom-div'>
+
+        <div className='unit-card'>
+          <h5>Small Unit</h5>
+          <div className='price-plan-deals'>
+          <img src="../src/assets/Dollar-icon.svg" alt="" />
+          <h3>50</h3>
+          <h6>/month</h6>
+          </div>
+          <p>Phasellus sollicitudin sapien eu massa accumsan, quis semper odio suscipit.</p>
+          <hr className='divider'/>
+          <div className="features-list">
+              <div className="feature-item">
+                <img src="../src/assets/checkmark-icon.svg" alt="checkmark" className="check-icon" />
+                <p>Nam nec ipsum in dolor</p>
+              </div>
+              <div className="feature-item">
+                <img src="../src/assets/checkmark-icon.svg" alt="checkmark" className="check-icon" />
+                <p>Fusce nec ligula ut arcu</p>
+              </div>
+              <div className="feature-item">
+                <img src="../src/assets/checkmark-icon.svg" alt="checkmark" className="check-icon" />
+                <p>Aliquam pulvinar arcu in</p>
+              </div>
+              <div className="feature-item">
+                <img src="../src/assets/checkmark-icon.svg" alt="checkmark" className="check-icon" />
+                <p>Duis gravida enim porta</p>
+              </div>
+              <div className="feature-item">
+                <img src="../src/assets/checkmark-icon.svg" alt="checkmark" className="check-icon" />
+                <p>Etiam eget libero non ligula</p>
+              </div>
+            </div>
+          <button className='discover-more-btn'>Rent Now</button>
+        </div>
+        <div className='unit-card'>
+          <h5>Medium Unit</h5>
+          <div className='price-plan-deals'>
+          <img src="../src/assets/Dollar-icon.svg" alt="" />
+          <h3>100</h3>
+          <h6>/month</h6>
+          </div>
+          <p>Phasellus sollicitudin sapien eu massa accumsan, quis semper odio suscipit.</p>
+          <hr className='divider'/>
+          <div className="features-list">
+              <div className="feature-item">
+                <img src="../src/assets/checkmark-icon.svg" alt="checkmark" className="check-icon" />
+                <p>Nam nec ipsum in dolor</p>
+              </div>
+              <div className="feature-item">
+                <img src="../src/assets/checkmark-icon.svg" alt="checkmark" className="check-icon" />
+                <p>Fusce nec ligula ut arcu</p>
+              </div>
+              <div className="feature-item">
+                <img src="../src/assets/checkmark-icon.svg" alt="checkmark" className="check-icon" />
+                <p>Aliquam pulvinar arcu in</p>
+              </div>
+              <div className="feature-item">
+                <img src="../src/assets/checkmark-icon.svg" alt="checkmark" className="check-icon" />
+                <p>Duis gravida enim porta</p>
+              </div>
+              <div className="feature-item">
+                <img src="../src/assets/checkmark-icon.svg" alt="checkmark" className="check-icon" />
+                <p>Etiam eget libero non ligula</p>
+              </div>
+            </div>
+          <button className='discover-more-btn'>Rent Now</button>
+        </div>
+        <div className='unit-card'>
+          <h5>Large Unit</h5>
+          <div className='price-plan-deals'>
+          <img src="../src/assets/Dollar-icon.svg" alt="" />
+          <h3>150</h3>
+          <h6>/month</h6>
+          </div>
+          <p>Phasellus sollicitudin sapien eu massa accumsan, quis semper odio suscipit.</p>
+          <hr className='divider'/>
+          <div className="features-list">
+              <div className="feature-item">
+                <img src="../src/assets/checkmark-icon.svg" alt="checkmark" className="check-icon" />
+                <p>Nam nec ipsum in dolor</p>
+              </div>
+              <div className="feature-item">
+                <img src="../src/assets/checkmark-icon.svg" alt="checkmark" className="check-icon" />
+                <p>Fusce nec ligula ut arcu</p>
+              </div>
+              <div className="feature-item">
+                <img src="../src/assets/checkmark-icon.svg" alt="checkmark" className="check-icon" />
+                <p>Aliquam pulvinar arcu in</p>
+              </div>
+              <div className="feature-item">
+                <img src="../src/assets/checkmark-icon.svg" alt="checkmark" className="check-icon" />
+                <p>Duis gravida enim porta</p>
+              </div>
+              <div className="feature-item">
+                <img src="../src/assets/checkmark-icon.svg" alt="checkmark" className="check-icon" />
+                <p>Etiam eget libero non ligula</p>
+              </div>
+            </div>
+          <button className='discover-more-btn'>Rent Now</button>
+        </div>
+        <div className='unit-card'>
+          <h5>Executive Unit</h5>
+          <div className='price-plan-deals'>
+          <img src="../src/assets/Dollar-icon.svg" alt="" />
+          <h3>200</h3>
+          <h6>/month</h6>
+          </div>
+          <p>Phasellus sollicitudin sapien eu massa accumsan, quis semper odio suscipit.</p>
+          <hr className='divider'/>
+          <div className="features-list">
+              <div className="feature-item">
+                <img src="../src/assets/checkmark-icon.svg" alt="checkmark" className="check-icon" />
+                <p>Nam nec ipsum in dolor</p>
+              </div>
+              <div className="feature-item">
+                <img src="../src/assets/checkmark-icon.svg" alt="checkmark" className="check-icon" />
+                <p>Fusce nec ligula ut arcu</p>
+              </div>
+              <div className="feature-item">
+                <img src="../src/assets/checkmark-icon.svg" alt="checkmark" className="check-icon" />
+                <p>Aliquam pulvinar arcu in</p>
+              </div>
+              <div className="feature-item">
+                <img src="../src/assets/checkmark-icon.svg" alt="checkmark" className="check-icon" />
+                <p>Duis gravida enim porta</p>
+              </div>
+              <div className="feature-item">
+                <img src="../src/assets/checkmark-icon.svg" alt="checkmark" className="check-icon" />
+                <p>Etiam eget libero non ligula</p>
+              </div>
+            </div>
+          <button className='discover-more-btn'>Rent Now</button>
+        </div>
+        
+      </div>
+
+    </div>
   )
 }
 
