a7d19e7d498b5f9cec3bf41e086e774fefdc7f75
Richard Karth <richard.k.triumph@gmail.com>
2025-11-06 13:06:41 +0100

    Finished styling the newsletter section

--- StorAidG/src/components/NewsLetter.jsx
+++ StorAidG/src/components/NewsLetter.jsx
@@ -2,7 +2,22 @@ import React from 'react'
 
 const NewsLetter = () => {
   return (
-    <div>NewsLetter</div>
+    <div className='news-letter-bg'>
+      <div className='margin-wrapper'>
+        <div className='news-letter-container'>
+          <div className='news-letter-left-side'>
+            <h3>Subscribe Our Newsletter</h3>
+            <p>Subscribe to our newsletter to receive early discount offers, updates and info</p>
+          </div>
+          <div className='news-letter-right-side'>
+            <form action="">
+              <input type="text" placeholder='Enter your email' required />
+              <button className='discover-more-btn'>Submit</button>
+            </form>
+          </div>
+        </div>
+      </div>
+    </div>
   )
 }
 
