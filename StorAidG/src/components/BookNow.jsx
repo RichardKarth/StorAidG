fd244942e77c49693185b5073b166a7ce6142a81
Richard Karth <richard.k.triumph@gmail.com>
2025-11-06 10:09:42 +0100

    Finished the booknow sections styling

--- StorAidG/src/components/BookNow.jsx
+++ StorAidG/src/components/BookNow.jsx
@@ -2,7 +2,16 @@ import React from 'react'
 
 const BookNow = () => {
   return (
-    <div>BookNow</div>
+    
+    
+    
+    <div className='booknow-bg'>
+      <div className='margin-wrapper'>
+        <h3>Looking for Secure and Flexible Storage? Find Your Perfect Fit With Us.</h3>
+        <p>Lorem ipsum dolor sit amet, consectetur adipiscing elit. Quisque molestie nisl sed dui lacinia gravida. Nulla quis nulla leo. Mauris ac blandit nisi non sodales augue. Phasellus eget elit gravida.</p>
+        <button className='discover-more-btn'>Book Now</button>
+      </div>
+    </div>
   )
 }
 
