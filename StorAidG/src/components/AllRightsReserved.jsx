da4e8309e5197d8eb87d6fe94775aeb990a9b818
Richard Karth <richard.k.triumph@gmail.com>
2025-11-06 13:22:08 +0100

    Finished styling the allrights section

--- StorAidG/src/components/AllRightsReserved.jsx
+++ StorAidG/src/components/AllRightsReserved.jsx
@@ -2,7 +2,28 @@ import React from 'react'
 
 const AllRightsReserved = () => {
   return (
-    <div>AllRightsReserved</div>
+    <div className='all-rights-reserved-bg'>
+      <div className='all-rights-wrapper'>
+        <div className='all-rights-container'>
+          <div>
+            <p>© 2025 StorAid, All rights reserved.</p>
+          </div>
+          <div className='all-rights-a-taggs'>
+            <div className='all-right-a-tagg-gap'>
+              <img src="../src/assets/Arrow-icon.svg" alt="" />
+              <a href="#">Terms & Conditions</a>
+            </div>
+
+            <div className='all-right-a-tagg-gap'>
+              <img src="../src/assets/Arrow-icon.svg" alt="" />
+              <a href="#">Privacy Policy</a>
+            </div>
+          </div>
+        </div>
+
+      </div>
+      
+    </div>
   )
 }
 
