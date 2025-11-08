cf52375ce4a59a8f94605b48064c004d7c30e341
Richard Karth <richard.k.triumph@gmail.com>
2025-11-08 09:40:08 +0100

    Added all but the FAQ components to Services-view

---
+++ StorAidG/src/components/ServicesViewHeader.jsx
@@ -0,0 +1,16 @@
+import React from 'react'
+
+const ServicesViewHeader = () => {
+  return (
+     <div className='about-hero-bg'>
+      <div className='padding-wrapper'>
+        <div className='about-hero-text'>
+            <h1>Services</h1>
+            <p>Lorem ipsum dolor sit amet, consectetur adipiscing elit. Ut elit tellus, luctus nec ullamcorper mattis, pulvinar dapibus leo.</p>
+        </div>
+      </div>
+    </div>
+  )
+}
+
+export default ServicesViewHeader
--- StorAidG/src/views/Services.jsx
+++ StorAidG/src/views/Services.jsx
@@ -1,8 +1,20 @@
 import React from 'react'
+import ServicesViewHeader from '../components/ServicesViewHeader'
+import OurServices from '../components/OurServices'
+import Testimonials from '../components/Testimonials'
+import BookNow from '../components/BookNow'
+import NewsLetter from '../components/NewsLetter'
 
 const Services = () => {
   return (
-    <div>Services</div>
+    <div>
+        <ServicesViewHeader/>
+        <OurServices/>
+        <Testimonials/>
+
+        <BookNow/>
+        <NewsLetter/>
+    </div>
   )
 }
 
