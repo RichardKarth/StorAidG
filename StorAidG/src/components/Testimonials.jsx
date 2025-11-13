49d96c4fdefe2c51c16e05dc991441fd2c2c6674
Richard Karth <richard.k.triumph@gmail.com>
2025-11-13 18:49:12 +0100

    Added and styled testimonials api data

--- StorAidG/src/components/Testimonials.jsx
+++ StorAidG/src/components/Testimonials.jsx
@@ -1,92 +1,78 @@
-import React from 'react'
+import React, { useEffect, useState } from 'react'
+
+
 
 const Testimonials = () => {
+  
+  
+  const [serviceItems, setServiceItems] = useState([])
+  
+  const fetchData = async () =>{
+    const res = await fetch ('https://win25-jsf-assignment.azurewebsites.net/api/testimonials')
+    const data = await res.json();
+    setServiceItems(data);
+  }
+  useEffect(() => {
+    fetchData()
+  }, [])
+  
+  console.log(serviceItems)
+
+
+  /* Fick hjälp av AI att mapa ut antalet stjärnor i functionen nedan. Längden på arrayen motsavarar det i item.rating och spannet "..." gör så att map kan räkna igenom tomma listan med undefined*/
+  
   return (
     <div className='testimonials-section'>
       <div className='margin-wrapper'>
           <div className='testimonials-wrapper'>
+
             <div className='testimonials-upper-box'>
               <h4>Testimonials</h4>
               <h3>See What Our Client Have to Say</h3>
               <p>Lorem ipsum dolor sit amet, consectetur adipiscing elit. Quisque molestie nisl sed dui lacinia gravida. Nulla quis nulla leo. Mauris ac blandit nisi non sodales augue. Phasellus eget elit gravida.</p>
 
             </div>
-
-
             <div className='testimonials-lower-box'>
-              {/* mapa ut strukturen för korten */}
+              {serviceItems.map((item) => {
+                return(
+                  <div key={item.id} className='testimonials-card'>
 
-              <div className='testimonials-lower-box-1'>
-                  <div className='testimonials-card-1 square testimonials-wrapper'>
-                      <div className='testimonials-grade'>
-                        
-                        <h2>****</h2>
+                    <div className='testimonials-rating'>
+                      <p>
+                        {[...Array(item.rating)].map((_,i) => (
+                          <span key={i} className="material-symbols-outlined">star</span>
+                        ))}
+                      </p>
+                    </div>
 
+                    <div className='testimonials-comment'>
+                      <p>{item.comment}</p>
+                    </div>
+
+
+                    <div className='name-and-picture'>
+                      <div className='testimonials-avatar-url'>
+                        <img src={item.avatarUrl} alt={item.name} />
                       </div>
-                      <div className='testimonial-comments'>
-                        <p>Lorem ipsum dolor sit amet, consectetur adipis cing elit. Ut libero lectus, porta nec turpis sit amet, lobortis fringilla ante.</p>
-                      </div>
-                      <div className='customer-info'>
-                        <div className='avatar-and-name'>
-                          <div className='customer-avatar'></div>
 
-                          <div className='customer-name'>
-                            <h6>Aiden Harvey</h6>
-                            <p>Customer</p>
+                        <div className='name-and-companyName'>
+                          <div className='testimonials-name'>
+                            <h6>{item.name}</h6>
+                          </div>
+
+                          <div className='testimonials-company-name'>
+                            <p>{item.companyName}</p>
                           </div>
                         </div>
 
-                        <div><img src="../src/assets/Quotation-marks.svg" alt="" /></div>
-
-                      </div>
+                      <div><img src="../src/assets/Quotation-marks.svg" alt="" /></div>
+                    </div>
                   </div>
-              </div>
-              <div className='testimonials-lower-box-2'>
-                  <div className='testimonials-card-2 square testimonials-wrapper'>
-                      <div className='testimonials-grade'>
-                        <h2>*****</h2>
-                      </div>
-                      <div>
-                        <p>Lorem ipsum dolor sit amet, consectetur adipis cing elit. Ut libero lectus, porta nec turpis sit amet, lobortis fringilla ante.</p></div>
-                      <div className='customer-info'>
-                        <div className='avatar-and-name'>
-                          <div className='customer-avatar'></div>
 
-                          <div className='customer-name'>
-                            <h6>Aiden Harvey</h6>
-                            <p>Customer</p>
-                          </div>
-                        </div>
 
-                        <div><img src="../src/assets/Quotation-marks.svg" alt="" /></div>
+                )
 
-                      </div>
-                  </div>
-              </div>
-              <div className='testimonials-lower-box-3'>
-                  <div className='testimonials-card-3 square testimonials-wrapper'>
-                      <div className='testimonials-grade'>
-                        <h2>*****</h2>
-                      </div>
-                      <div className=''>
-                        <p>Lorem ipsum dolor sit amet, consectetur adipis cing elit. Ut libero lectus, porta nec turpis sit amet, lobortis fringilla ante.</p>
-                      </div>
-
-                      <div className='customer-info'>
-                        <div className='avatar-and-name'>
-                          <div className='customer-avatar'></div>
-
-                          <div className='customer-name'>
-                            <h6>Aiden Harvey</h6>
-                            <p>Customer</p>
-                          </div>
-                        </div>
-
-                        <div><img src="../src/assets/Quotation-marks.svg" alt="" /></div>
-
-                      </div>
-                  </div>
-              </div>
+              })}
             </div>
           </div>
       </div>
